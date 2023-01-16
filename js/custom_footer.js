11352ec30eb3a6299f7da4358a23f243be1fe6fd
Sebastian Cherny <sebastiancherny@Sebastians-MacBook-Air.local>
2023-01-16 00:17:49 -0300

    Improvements

--- js/custom_footer.js
+++ js/custom_footer.js
@@ -10,17 +10,6 @@ class CustomFooter extends HTMLElement {
                 <p class="footer-company-name">© 2023 Sebastian Cherny</p>
             </div>
             <div class="footer-center">
-
-                <div>
-                    <i class="fa fa-map-marker"></i>
-                    <p>Argentina</p>
-                </div>
-
-                <div>
-                <i class="fa fa-envelope"></i>
-                <p><a style="color: white;" href="mailto:sebascherny@gmail.com">sebascherny@gmail.com</a></p>
-                </div>
-
             </div>
 
             <div class="footer-right">
