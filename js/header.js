c049d9bccf7a9a593900f8b72310ee6793f8ffd1
Sebastian Cherny <sebastiancherny@Sebastians-MacBook-Air.local>
2023-01-22 00:54:37 -0300

    .

--- js/header.js
+++ js/header.js
@@ -15,7 +15,7 @@ class Header extends HTMLElement {
                     </h2>
                 </div>
                 <div id="headerButtonsDiv">
-                    <a class="headerButton" href="index.html#projects">work</a>
+                    <a class="headerButton" href="index.html#latest-projects">work</a>
                     <a class="headerButton" href="about.html">about</a>
                     <a class="headerButton" href="contact.html">contact</a>
                 </div>
