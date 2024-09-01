7b4da9ed0586df06c582b5681a958c3ccfadab10
Sebastian Cherny <sebascherny@gmail.com>
2024-09-01 03:53:44 +0200

    Updating CV; Adding services; Adding multiple recent projects

--- js/header.js
+++ js/header.js
@@ -17,6 +17,7 @@ class Header extends HTMLElement {
                 <div id="headerButtonsDiv">
                     <a class="headerButton" href="index.html#latest-projects">work</a>
                     <a class="headerButton" href="about.html">about</a>
+                    <a class="headerButton" href="index.html#services">services</a>
                     <a class="headerButton" href="contact.html">contact</a>
                 </div>
             </div>
